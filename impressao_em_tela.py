c9cdfa72f9d50b698d8a381ad5bf61e140dabde3
fernandavss <116000072+fernandavss@users.noreply.github.com>
2022-11-12 15:45:27 -0300

    add comentario

--- impressao_em_tela.py
+++ impressao_em_tela.py
@@ -214,7 +214,7 @@ while recomeco:
                                             jogando = False
                                             recomeco = False
 
-                    #errado
+                    #errado a alternativa
                     elif resposta_perg in alternativas:
                         print('perdeu tudo :(')
                         cont = input('quer continuar jogando [S/N]?')
@@ -229,7 +229,7 @@ while recomeco:
                     
 
 
-                    #outro
+                    #outro (resposta aleatoria)
                     else:
                         print('Opção inválida!\n{0}ERRO RUDE{1}\nAs opções de resposta são: {2}A,B,C,D, ajuda, pula e sair{3}'.format('\033[1;31m','\033[m','\033[1;36m','\033[m'))
                         resposta_perg = input('resposta:')
