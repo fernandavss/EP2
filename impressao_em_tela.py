add013fbb30789a7eec837c154b2c9cda464b2fc
fernandavss <116000072+fernandavss@users.noreply.github.com>
2022-11-12 15:41:44 -0300

    correcao de texto

--- impressao_em_tela.py
+++ impressao_em_tela.py
@@ -231,7 +231,7 @@ while recomeco:
 
                     #outro
                     else:
-                        print('Opção inválida! \n{0}ERRO RUDE{1}\n As opções de resposta são: {2}A,B,C,D, ajuda, pula e sair{3}'.format('\033[1;31m','\033[m','\033[1;36m','\033[m'))
+                        print('Opção inválida!\n{0}ERRO RUDE{1}\nAs opções de resposta são: {2}A,B,C,D, ajuda, pula e sair{3}'.format('\033[1;31m','\033[m','\033[1;36m','\033[m'))
                         resposta_perg = input('resposta:')
 
 
